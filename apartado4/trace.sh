f9f55c47fb158945ab75657d7135cf23c67ef7ce
dariofdezl <43450002+dariofdezl@users.noreply.github.com>
2018-11-01 02:59:52 +0100

    Update apartado4/trace.sh

---
+++ apartado4/trace.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+source /opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0\
+/openmpi/2.1.1/extrae/3.5.2/etc/extrae.sh
+export EXTRAE_CONFIG_FILE=${HOME}/extrae.xml
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitrace.so # C code
+$* # Run the program
