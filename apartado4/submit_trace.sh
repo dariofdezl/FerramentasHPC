c2001b8368745dac01ed221e40df843b4c76993e
dariofdezl <43450002+dariofdezl@users.noreply.github.com>
2018-11-04 21:08:15 +0100

    Update apartado4/submit_trace.sh

--- apartado4/submit_trace.sh
+++ apartado4/submit_trace.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH -t 10:00:00 # execution time hh:mm:ss *OB*
+#SBATCH -t 00:01:00 # execution time hh:mm:ss *OB*
 #SBATCH -n 16 #tasks (MPI processes)
 #SBATCH -c 24 #cores/task (shared-mem threads/process)
 ##SBATCH -N 16 #nodes
 #SBATCH -p cola-corta
-
+source env.sh
 module load gcc/6.4.0
 module load openmpi/2.1.1 extrae/3.5.2
-# export TRACE_NAME=trace_filename.prv # *OP*
-srun -n 16 ${HOME}/trace.sh ./dgesv 4096
+export TRACE_NAME=trace_filename.prv # *OP*
+srun -n 16 ${HOME}/trace.sh ./dgesv 2048
